d1bd13902a8c2cb3a8477e1e675f8ae744006f84
lin <jlin-z@qq.com>
2020-12-28 22:31:53 +0800

    register and login

---
+++ api/user.js
@@ -0,0 +1,17 @@
+import request from "@/utils/request"
+
+export const login = (data) => {
+  return request({
+    url: '/api/users/login',
+    method: 'POST',
+    data
+  })
+}
+
+export const register = (data) => {
+  return request({
+    url: '/api/users',
+    method: 'POST',
+    data
+  })
+}
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -11,18 +11,20 @@
 					</p>
 
 					<ul class="error-messages">
-						<li>That email is already taken</li>
+						<template v-for="(messages, field) in errors">
+							<li v-for="(message, index) in messages" :key="index">{{ field }} {{ message }}</li>
+						</template>
 					</ul>
 
-					<form>
+					<form @submit.prevent="onSubmit">
 						<fieldset v-if="!isLogin" class="form-group">
 							<input class="form-control form-control-lg" type="text" placeholder="Your Name">
 						</fieldset>
 						<fieldset class="form-group">
-							<input class="form-control form-control-lg" type="text" placeholder="Email">
+							<input v-model="user.email" class="form-control form-control-lg" type="email" placeholder="Email" required>
 						</fieldset>
 						<fieldset class="form-group">
-							<input class="form-control form-control-lg" type="password" placeholder="Password">
+							<input v-model="user.password" class="form-control form-control-lg" type="password" placeholder="Password">
 						</fieldset>
 						<button class="btn btn-lg btn-primary pull-xs-right">
 							{{ isLogin ? 'Sign in' : 'Sign up' }}
@@ -36,11 +38,42 @@
 </template>
 
 <script>
+import { login, register } from "@/api/user";
+
 export default {
 	name: "LoginIndex",
+	data () {
+		return {
+			user: {
+				email: '',
+				password: ''
+			},
+			errors: {
+
+			}
+		}
+	},
 	computed: {
 		isLogin () {
 			return this.$route.name === 'login'
+		}
+	},
+	methods: {
+		async onSubmit () {
+			try {
+				const { data } = this.islogin
+				? await login({
+						user: this.user
+					})
+				: await register({
+						user: this.user
+					})
+
+				this.$router.push('/')
+			} catch (err) {
+				this.errors = err.response.data.errors
+				console.log(this.errors)
+			}
 		}
 	}
 };
